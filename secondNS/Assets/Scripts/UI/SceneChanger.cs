a5d332ecd84e4fb10224d9e01af58be70770b999
Коля Химич <kylturpro@gmail.com>
2022-04-17 17:37:30 +0300

    SceneChange

--- secondNS/Assets/Scripts/UI/SceneChanger.cs
+++ secondNS/Assets/Scripts/UI/SceneChanger.cs
@@ -1,20 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    GameObject hidergo;
     SpriteRenderer hider;
+
+    GameObject lockpanel;
+    GameObject lockpanelobj;
+
+    float hidetime = 0.5f;
+    float opentime = 0.5f;
+
+    string SceneName;
+    bool hiding = false;
+    bool opening = false;
+    float time = 0;
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        lockpanel = Resources.Load<GameObject>("Panel");
         hider = GetComponentInChildren<SpriteRenderer>();
-        hidergo = GetComponentInChildren<Transform>().gameObject;
     }
     public void Changescene(string SceneName)
     {
-
+        this.SceneName = SceneName;
+        hiding = true;
+        Instantiate(lockpanel, GameObject.Find("Canvas").transform);
+    }
+    private void Update()
+    {
+        if (hiding)
+        {
+            time += Time.deltaTime;
+            hider.color = new Color(hider.color.r, hider.color.g, hider.color.b, time/ hidetime);
+            if (time >= hidetime)
+            {
+                hiding = false;
+                SceneManager.LoadScene(SceneName);
+                lockpanelobj = Instantiate(lockpanel, GameObject.Find("Canvas").transform);
+                opening = true;
+                time = 0;
+            }
+        }
+        if (opening)
+        {
+            time += Time.deltaTime;
+            hider.color = new Color(hider.color.r, hider.color.g, hider.color.b, Mathf.Abs(1 - time / hidetime));
+            if (time - opentime >= 0)
+            {
+                opening = false;
+                hider.color = new Color(hider.color.r, hider.color.g, hider.color.b, 0);
+                time = 0;
+                Destroy(lockpanelobj);
+            }
+        }
     }
 }
